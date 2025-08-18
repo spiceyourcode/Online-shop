9c5e52222932ebf916e9871e24d852e46b2859da
spicyourcode <franklineomari.com>
2025-08-18 08:52:54 +0300

    creating the cart test

--- backend/carts/models.py
+++ backend/carts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+#  This is the cart model - one per user 
+class Cart(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="cart")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"cart of {self.user.username}"
+
+    @property
+    def total_price(self):
+        # calculating the total price
+        return sum(item.subtotal for item in self.items.all())
+    
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+
+    class Meta:
+        unique_together = ("cart", "product") #this is to avoid duplicate items
+
+    def  __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+    
+    @property 
+    def subtotal(self):
+        #calculating the subtotal p * q
+        return self.product.price *self.quantity
+    
+
+
--- backend/carts/tests.py
+++ backend/carts/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from products.models import Product, Category
+from .models import Cart, CartItem
 
-# Create your tests here.
+User = get_user_model()
+
+class CartModelTest(TestCase):
+    def setUp(self):
+        # Creating a test user 
+        self.user = User.objects.create_user(
+            email='testuser@example.com',
+            password='password123',
+            is_seller=True
+        )
+        
+        # Create a category first
+        self.category = Category.objects.create(name="Electronics", slug="electronics")
+        
+        # creating a test product 
+        self.product = Product.objects.create(
+            name="laptop",
+            price=1000,
+            category=self.category,
+            seller=self.user
+        )
+        
+        # create a cart for user 
+        self.cart = Cart.objects.create(user=self.user)
+
+    def test_add_cart_item(self):
+        # add product to cart 
+        item = CartItem.objects.create(cart=self.cart, product=self.product, quantity=2)
+        self.assertEqual(item.subtotal, 2000)
+        self.assertEqual(self.cart.total_price, 2000)
+
+    def test_unique_cart_item(self):
+        # add the items once
+        CartItem.objects.create(cart=self.cart, product=self.product, quantity=1)
+        # attempt duplicate
+        with self.assertRaises(Exception):
+            CartItem.objects.create(cart=self.cart, product=self.product, quantity=3)
