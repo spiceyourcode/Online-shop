92efabfad7eee41eed4498fc22db9aaf19fe39dc
spiceyourcode <franklineomari@gmail.com>
2025-09-11 18:33:16 +0300

    adding the admin config in the orders module

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user","address","total_price","created_at")
+    list_filter = ("is_paid","created_at")
+    search_fields =("user__email",)
+    inlines  = [OrderItemInline]
