8ce7f4d519f9debfba90f284a9f1b18ee17d9490
spiceyourcode <franklineomari@gmail.com>
2025-09-12 19:58:08 +0300

    added the admin panels to the carts payments and reviews modules

--- backend/carts/admin.py
+++ backend/carts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0 
+    
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+    inlines = [CartItemInline]
