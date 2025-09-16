78d0f2c32b9b9b1147a7cc64374bcc1344e367b8
spiceyourcode <franklineomari@gmail.com>
2025-09-17 01:35:42 +0300

    Added react-router-dom and implemented routing for the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css';
-function App() {
+import {BrowserRouter as Router, Routes ,Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
-  return (
-
-  )
+function App(){
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/Register" element = {<Register/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
