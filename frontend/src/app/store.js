4859d380ad902b4ffcbd26aafd221ad9a5715a1e
spiceyourcode <franklineomari@gmail.com>
2025-09-22 17:28:54 +0300

    addingthe product reducer in the store.js and modifying the Home app

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";  //this is the store to hold the app state
 import authReducer from "../features/authSlice"; 
+import productsReducer from "../features/productsSlice"
 //reducer - function that updtaes states based on actions 
 
 
@@ -7,5 +8,6 @@ export const store = configureStore({
     reducer: {
         // This manages authentication state (login/loggedout)
         auth : authReducer,
+        products: productsReducer,
     },
 });
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,31 @@
-function Home(){
-    return(
-        <>
-        <h1>
-            Welcome to Purchase with us!!!
-        </h1>
-        </>
-    );
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../features/productsSlice";
+import { Link } from "react-router-dom";
+
+function Home() {
+  const dispatch = useDispatch();
+  const { items, status } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (status === "loading") return <div> Loading ...</div>;
+  if (!items.length) return <p>No products found</p>;
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {items.map((item) => {
+          <li key={item.id}>
+            <Link to={`/products/${item.id}`}>
+              {item.name} -{item.price}
+            </Link>
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
 }
-export default Home;
